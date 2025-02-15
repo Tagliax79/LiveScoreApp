761ff9cdaca9668cf396e6ff35ca75cb4dcf9217
Tagliax79 <tagliax79@gmail.com>
2025-02-15 18:59:25 +0100

    Ripristino codice funzionante

--- api.py
+++ api.py
@@ -41,7 +41,43 @@ def fetch_match_details(match_id):
     except json.JSONDecodeError:
         return {"error": "Errore nella decodifica della risposta API"}
 
-# Funzione per ottenere gli incidenti della partita
+# Funzione per ottenere le statistiche della partita
+def fetch_match_statistics(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/statistics"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
+
+# Funzione per ottenere i marcatori della partita (se esiste)
+def fetch_match_scorers(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/highlights"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
+
+# Funzione per ottenere gli incidenti della partita (se esiste)
 def fetch_match_incidents(match_id):
     conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
     headers = {
@@ -59,7 +95,7 @@ def fetch_match_incidents(match_id):
     except json.JSONDecodeError:
         return {"error": "Errore nella decodifica della risposta API"}
 
-# Funzione per ottenere il logo della competizione (RESTITUISCE IL LINK DIRETTO)
+# Funzione per ottenere il logo di un torneo
 def fetch_tournament_logo(tournament_id):
+    # Restituisce il link diretto all'immagine
     return f"https://allsportsapi2.p.rapidapi.com/api/tournament/{tournament_id}/image"
-
