6347e4031cc4f008fac7366b55465ac611c83ff7
Tagliax79 <tagliax79@gmail.com>
2025-02-15 15:55:43 +0100

    ripristino versione funzionante

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")  # Usa la chiave API dai secrets
 
 # Funzione per ottenere le partite in corso
 def fetch_live_matches():
-    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)  
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
     headers = {
         'x-rapidapi-key': RAPIDAPI_KEY,
         'x-rapidapi-host': RAPIDAPI_HOST
@@ -59,6 +59,38 @@ def fetch_match_statistics(match_id):
     except json.JSONDecodeError:
         return {"error": "Errore nella decodifica della risposta API"}
 
-# Funzione per ottenere il logo della competizione
-def fetch_tournament_logo(tournament_id):
-    return f"https://allsportsapi2.p.rapidapi.com/api/tournament/{tournament_id}/image"
+# Funzione per ottenere i marcatori della partita
+def fetch_match_scorers(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/highlights"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
+
+# 🔹 **Nuova funzione per ottenere gli incidenti della partita**
+def fetch_match_incidents(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/incidents"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
