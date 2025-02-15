fe8272fc6e1474cad923d4766cdf72475f719b93
Tagliax79 <tagliax79@gmail.com>
2025-02-15 15:36:36 +0100

    Fix ImportError: aggiunta fetch_match_statistics in api.py

--- api.py
+++ api.py
@@ -41,7 +41,61 @@ def fetch_match_details(match_id):
     except json.JSONDecodeError:
         return {"error": "Errore nella decodifica della risposta API"}
 
-# Funzione per ottenere il logo della competizione
+# **Funzione per ottenere le statistiche della partita**
+def fetch_match_statistics(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/statistics"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
+
+# **Funzione per ottenere i marcatori della partita**
+def fetch_match_scorers(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/highlights"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
+
+# **Funzione per ottenere gli incidenti della partita**
+def fetch_match_incidents(match_id):
+    conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
+    headers = {
+        'x-rapidapi-key': RAPIDAPI_KEY,
+        'x-rapidapi-host': RAPIDAPI_HOST
+    }
+
+    endpoint = f"/api/match/{match_id}/incidents"
+    conn.request("GET", endpoint, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    try:
+        return json.loads(data.decode("utf-8"))
+    except json.JSONDecodeError:
+        return {"error": "Errore nella decodifica della risposta API"}
+
+# **Funzione per ottenere il logo della competizione**
 def fetch_tournament_logo(tournament_id):
     conn = http.client.HTTPSConnection(RAPIDAPI_HOST)
     headers = {
